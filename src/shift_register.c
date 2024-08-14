c4c3a44f0965b792c9ee27ebcaca13a72c60afb3
Lu-Die-Milchkuh <zebrowskylucas@proton.me>
2024-08-14 16:06:15 +0200

    Fix Shift Logic

--- src/shift_register.c
+++ src/shift_register.c
@@ -1,22 +1,39 @@
-#include <stdio.h>
-
-#include "cpu.h"
 #include "shift_register.h"
+#include <stdint.h>
+// #include <stdio.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
 static uint16_t shiftRegister = 0;
 static uint8_t shiftOffset = 0;
 
 void setShiftRegister(uint8_t data)
 {
-	shiftRegister = shiftRegister >> (8 - shiftOffset) | data << 8;
+	shiftRegister = (shiftRegister >> 8) | (data << 8);
 }
 
 void setShiftOffset(uint8_t offset)
 {
-	shiftOffset = offset;
+	shiftOffset = offset & 0x7;
 }
 
+// TODO: This needs to be looked at
 uint8_t getShiftRegister(void)
 {
-	return shiftRegister >> 8;
+	uint16_t result = shiftRegister >> (8 - shiftOffset);
+	// printf("Shift Register: %x, Offset: %x, Result: %x\n", shiftRegister,
+	// 	   shiftOffset, result);
+	return (uint8_t)(result & 0xFF);
+
+	// uint16_t mask = 0xFF00 >> shiftOffset;
+
+	// uint16_t result = shiftRegister & mask;
+
+	// if (shiftRegister != 0 && shiftOffset != 0) {
+	// 	printf("Data: %x, Offset: %x, result: %x (Calc: %x)\n", shiftRegister,
+	// 		   shiftOffset, result, (uint8_t)(result >> (8 - shiftOffset)));
+	// 	exit(1);
+	// }
+
+	// return (uint8_t)(result >> (8 - shiftOffset));
 }
